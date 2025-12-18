f7e8ce0410051c637640f304e467ac96a820db85
Connor Ivy <connor@MacBook-Pro.local>
2025-12-18 10:47:55 -0600

    rename more files

--- rename_and_update.py
+++ rename_and_update.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 json_path = 'HraBot.Api/Data/intercom_help_files.json'
 data_dir = 'HraBot.Api/Data'
@@ -8,15 +9,17 @@ with open(json_path, 'r') as f:
     data = json.load(f)
 
 files = set(os.listdir(data_dir))
+# create a map of cleaned file names to original file names
+file_map = {re.sub(r'[^a-zA-Z0-9 ]', '', entry['blogPostTitle']).strip() + '.md': entry for entry in data}
 count = 0
 for entry in data:
     if not entry.get('renamed', False) and count < 30:
-        old = entry['blogPostTitle'].strip().replace('?', '') + '.md'
+        old = re.sub(r'[^a-zA-Z0-9 ]', '', entry['blogPostTitle']).strip() + '.md'
         if 'articles/' in entry['link']:
             new = entry['link'].split('articles/')[1] + '.md'
-            old_path = os.path.join(data_dir, old)
             new_path = os.path.join(data_dir, new)
-            if old in files and not os.path.exists(new_path):
+            if old in file_map.keys() and not os.path.exists(new_path):
+                old_path = os.path.join(data_dir, file_map[old])
                 print(f'Renaming: {old} -> {new}')
                 os.rename(old_path, new_path)
                 entry['renamed'] = True
